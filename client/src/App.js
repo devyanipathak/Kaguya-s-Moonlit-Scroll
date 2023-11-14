76ebd6d37a1893af0ef3817bb4b39e078ef7265b
devyanipathak <devyanipthk@gmail.com>
2023-11-14 07:03:53 +0530

    modified

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+//components 
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+import Description from './components/Description';
+function App() {
+  return (
+    <div>
+      <Header />
+      <TodoForm />
+      <Description />
+    </div>
+  );
+}
+
+export default App;
